207380691f25f1835501a0f2b8e3527ee0b8e5b8
unknown <monsuralam7262@gmail.com>
2021-08-02 12:55:16 +0600

    maintained  some code

--- index.js
+++ index.js
@@ -9,9 +9,6 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.get('/', (req, res) => {
-    res.send('Reduce Fluffy site is under development');
-});
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.8aajs.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
@@ -19,6 +16,15 @@ client.connect((err) => {
     const collection = client.db('test').collection('devices');
     // client.close();
 });
+
+
+app.get('/', (req, res) => {
+    res.send('Reduce Fluffy site is under development');
+});
+
+app.get('/questions', (req, res) => {
+    res.send('Public have no questions about us!!');
+})
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
