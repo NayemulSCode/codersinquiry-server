f41a0c8325594a95d8e0c0f6f16143dbf91f271e
mustaquenadim <mustaquenadim2000@gmail.com>
2021-08-26 00:35:59 +0600

    fixing errors

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ import mongoose from 'mongoose';
 import cors from 'cors';
 import config from './config.js';
 import authRouter from './routes/auth.js';
-const filesUpload = require('express-fileupload')
+// const filesUpload = require('express-fileupload');
 
-import {MongoClient} from 'mongodb';
+import { MongoClient } from 'mongodb';
 import dotenv from 'dotenv';
 import { count } from 'console';
-app.use(filesUpload())
+// app.use(filesUpload());
 dotenv.config();
 // db connection
 // const mongodbUrl = config.MONGODB_URL;
@@ -22,98 +22,73 @@ dotenv.config();
 //   })
 //   .then(()=>console.log('db connected.'))
 //   .catch((error) => console.log(error.message));
-  
-  // middleware
-  const app = express();
-  app.use(cors());
-  app.use(express.json());
-  app.use(express.urlencoded({ extended: false }));
 
-  // app.use("/api/users",authRouter);
+// middleware
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// app.use("/api/users",authRouter);
 
 const uri = `mongodb+srv://reduceFluffy:reduceFluffy072021@cluster0.8aajs.mongodb.net/reduceFluffy?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect((err) => {
     console.log('error:', err);
     const usersCollection = client.db('reduceFluffy').collection('users');
-    const courseCollection = client.db('reduceFluffy').collection('courses');
+    const questionsCollection = client.db('reduceFluffy').collection('questions');
+    const articlesCollection = client.db('reduceFluffy').collection('articles');
 
+    /*** POST ***/
+    // post user
+    app.post('/users', (req, res) => {
+        const user = req.body;
+        console.log(user);
+        usersCollection.insertOne(user).then((result) => {
+            res.send(result.insertedCount > 0);
+            console.log(result);
+        });
+    });
 
-    //create service order
-    app.post('/users',(req, res)=>{
-      const user = req.body;
-      console.log(user)
-      usersCollection.insertOne(user)
-      .then( result =>{
-          res.send(result.insertedCount > 0);
-          console.log(result)
-      })
-    })
+    // post question
+    app.post('/addQuestion', (req, res) => {
+        questionsCollection.insertOne(req.body).then((result) => {
+            res.send(result.insertedCount > 0);
+            console.log(result);
+        });
+    });
+
+    /*** GET ***/
     // get all users
-    app.get('/user',(req, res)=>{
-      usersCollection.find({})
-      .toArray((err, results) =>{
-        res.send(results);
-      })
-    })
-    console.log("db connected!!");
+    app.get('/user', (req, res) => {
+        usersCollection.find({}).toArray((err, results) => {
+            res.send(results);
+        });
+    });
 
-    
- //AllCourses
-  app.post('/all', (req, res) => {
-    const heading = req.body.heading;
-    const code= req.body.code;
-    const details = req.body.details;
-     courseCollection.insertOne({heading, code, details})
-    .then(getResult => {
-      res.send(getResult.insertedCount > 0)
-    })
-  })
+    // get all questions
+    app.get('/questions', (req, res) => {
+        questionsCollection.find({}).toArray((err, results) => {
+            res.send(results);
+        });
+    });
 
-app.get('/getJavascript', (req, res) => {
-  courseCollection.find().limit(20).toArray((err, js) => {
-    res.send(js)
-  })
-})
-
-
-app.get('/getPython', (req, res) => {
-  courseCollection.find().skip(20).limit(20).toArray((err, python) => {
-    res.send(python)
-  })
-})
-
-app.get('/getReactJs', (req, res) => {
-  courseCollection.find().skip(40).toArray((err, react) => {
-    res.send(react)
-  })
-})
-
-
-
-// End All courses
-
-
-
-
-
-
-
-
+    // get all articles
+    app.get('/articles', (req, res) => {
+        articlesCollection.find({}).toArray((err, results) => {
+            res.send(results);
+        });
+    });
 });
-
 
 app.use((err, req, res, next) => {
     res.status(500).send({ message: err.message });
-  });
+});
 
 app.get('/', (req, res) => {
     res.send('Reduce Fluffy site is under development');
 });
 
-app.get('/questions', (req, res) => {
-    res.send('Public have no questions about us!!');
-})
-const port = 5000
+const port = 5000;
 const PORT = process.env.PORT || port;
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
